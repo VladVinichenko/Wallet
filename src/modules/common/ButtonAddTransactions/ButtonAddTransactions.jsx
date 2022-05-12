58be94f67a7a9375034a5a4bb78aba610650c61a
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2022-05-12 22:08:50 +0300

    add cursor pointer

--- src/modules/common/ButtonAddTransactions/ButtonAddTransactions.jsx
+++ src/modules/common/ButtonAddTransactions/ButtonAddTransactions.jsx
@@ -2,14 +2,10 @@ import { IoMdAddCircle } from 'react-icons/io'
 
 import style from './ButtonAddTransactions.module.css'
 
-function ButtonAddTransactios() {
+export default function ButtonAddTransactios() {
 	return (
-		<>
-			<button type='button' className={style.addTransactios}>
-				<IoMdAddCircle className={style.icon} />
-			</button>
-		</>
+		<button type='button' className={style.addTransactios}>
+			<IoMdAddCircle className={style.icon} />
+		</button>
 	)
 }
-
-export default ButtonAddTransactios
