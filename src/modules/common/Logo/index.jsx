de0d5e76f0cee820d7604dcbef63f7921ff61a59
VladVinichenko <vinnyxxxl925@gmail.com>
2022-05-13 23:54:01 +0300

    fix import

--- src/modules/common/Logo/index.jsx
+++ src/modules/common/Logo/index.jsx
@@ -1,46 +1,46 @@
 import styled from 'styled-components'
-import { vars } from '../../../stylesheet'
+import { vars } from 'stylesheet'
 import { sprite } from 'assets'
 
 const Wrapper = styled.div`
-    display: flex;
+	display: flex;
 `
 
 const LogoSvg = styled.svg`
-    width: 30px;
-    height: 30px;
-    margin: 15px 15px 15px 0;
+	width: 30px;
+	height: 30px;
+	margin: 15px 15px 15px 0;
 
-    @media screen and (min-width: ${vars.breakpoints.tablet}) {
-        width: 40px;
-        height: 40px;
-        margin: 20px 20px 20px 0;
-    }
+	@media screen and (min-width: ${vars.breakpoints.tablet}) {
+		width: 40px;
+		height: 40px;
+		margin: 20px 20px 20px 0;
+	}
 `
 
 const LogoName = styled.span`
-    display: flex;
-    align-items: center;
-    
-    font-family: 'Poppins-Bold', sans-serif;
-    font-style: normal;
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 1.5;
+	display: flex;
+	align-items: center;
 
-    color: ${vars.color.font.primary};
+	font-family: 'Poppins-Bold', sans-serif;
+	font-style: normal;
+	font-weight: 700;
+	font-size: 18px;
+	line-height: 1.5;
 
-    @media screen and (min-width: ${vars.breakpoints.tablet}) {
-        font-size: 30px;
-        line-height: 1.5;
-    }
+	color: ${vars.color.font.primary};
+
+	@media screen and (min-width: ${vars.breakpoints.tablet}) {
+		font-size: 30px;
+		line-height: 1.5;
+	}
 `
 
 export const Logo = () => (
-    <Wrapper>
-        <LogoSvg width="40" height="40">
-            <use href={`${sprite}#icon-wallet`}></use>
-        </LogoSvg>
-        <LogoName>Wallet</LogoName>
-    </Wrapper>
+	<Wrapper>
+		<LogoSvg width='40' height='40'>
+			<use href={`${sprite}#icon-wallet`}></use>
+		</LogoSvg>
+		<LogoName>Wallet</LogoName>
+	</Wrapper>
 )
