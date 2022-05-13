c70bdf9cc3b3ae4ffd1b8c5a1bd631b084673e97
Viktor Sinelnykov <Fenya.ua@gmail.com>
2022-05-13 17:40:15 +0300

    feature/modal-component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { GlobalStyle, Fonts } from './stylesheet'
-import { Fragment } from 'react'
+import { useState, Fragment } from 'react'
+import Modal from 'modules/components/Modal'
 
 function App() {
+	const [isModalOpen, setIsModalOpen] = useState(false)
+
+	const toggleModal = () => {
+		setIsModalOpen(!isModalOpen)
+	}
+
 	return (
 		<Fragment>
 			<Fonts />
 			<GlobalStyle />
+			{isModalOpen && <Modal onClose={toggleModal}></Modal>}
 		</Fragment>
 	)
 }
