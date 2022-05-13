8cd0aee5e10365f86a844bfc99b1fad359efe650
tikkasandy <82177700+tikkasandy@users.noreply.github.com>
2022-05-13 23:26:11 +0300

    Add comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Fragment } from 'react'
 // import { ButtonAddTransactios } from 'modules'
-import { Logo } from 'modules'
+// import { Logo } from 'modules'
 
 export default function App() {
 	return <Fragment>
 		{/* <ButtonAddTransactios /> */}
-		<Logo />
+		{/* <Logo /> */}
 	</Fragment>
 }
