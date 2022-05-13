d4518e238b09982de27044b8bdd3a05a5f11eafd
VladVinichenko <vinnyxxxl925@gmail.com>
2022-05-13 09:39:20 +0300

    add vars

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { GlobalStyle, Fonts } from './stylesheet'
 import { Fragment } from 'react'
+import styled from 'styled-components'
+import { vars } from './stylesheet'
+
+const Meow = styled.div`
+	heigh: 200px;
+	width: 200px;
+	background-color: ${vars.color.background.card};
+`
 
 function App() {
 	return (
 		<Fragment>
 			<Fonts />
 			<GlobalStyle />
+			<Meow />
 		</Fragment>
 	)
 }
--- src/stylesheet/vars.js
+++ src/stylesheet/vars.js
@@ -1,5 +1,60 @@
 export const vars = {
 	color: {
-		primaryBackgroundColor: '#ffffff',
+		background: {
+			primary: '#FFFFFF',
+			seconary: '#E7EAF2',
+			page: 'rgba(255, 255, 255, 0.4)',
+			card: '#4A56E2',
+			cardGradient: 'linear-gradient(180deg, rgba(255, 255, 255, 0.2) -8.67%, rgba(255, 255, 255, 0) 116.22%)',
+		},
+		font: {
+			primary: '#000000',
+			seconary: '#A6A6A6',
+			third: '#BDBDBD',
+			fourth: '#4A56E2',
+			positive: '#24CCA7',
+			negative: '#FF6596',
+		},
+		accent: {
+			primary: '#24CCA7',
+			seconary: '#4A56E2',
+			fourth: '#6E78E8',
+			firstEllipse: '#FFD8D0',
+			secondEllipse: '#C5BAFF',
+		},
+		line: {
+			boxShadow: 'rgba(255, 255, 255, 0.6)',
+		},
+		chart: {
+			basic: '#FED057',
+			products: '#FFD8D0',
+			car: '#FD9498',
+			careAbout: '#C5BAFF',
+			careChild: '#6E78E8',
+			forHome: '#4A56E2',
+			education: '#81E1FF',
+			leisure: '#24CCA7',
+			other: '#00AD84',
+		},
+	},
+	filter: {
+		pageBackgroundFilter: 'blur(50px)',
+	},
+	animation: {
+		duration: '250ms',
+	},
+	borderRadius: {
+		primary: '20px',
+		seconary: '30px',
+		third: '2px',
+	},
+	border: {
+		firstLine: '1px solid #DCDCDF',
+		secondLine: '1px solid #E7E5F2',
+		thirdLine: `1px solid ${this.color.font.third}`,
+	},
+	boxShadow: {
+		firstLine: `0px 1px 0px ${this.color.line.boxShadow}`,
+		secondLine: `-1px 0px 0px rgba(0, 0, 0, 0.05), 1px 0px 0px ${this.color.line.boxShadow}`,
 	},
 }
