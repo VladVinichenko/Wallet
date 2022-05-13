96677254581a30b297744cbd07baf488cd2e3d72
VladVinichenko <vinnyxxxl925@gmail.com>
2022-05-13 11:43:32 +0300

    del cont

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import { GlobalStyle, Fonts } from './stylesheet'
 import { Fragment } from 'react'
-import { Container } from 'modules'
-import styled from 'styled-components'
-
-const Div = styled.div`
-	width: 100%;
-	height: 800px;
-	background-color: red;
-`
 
 function App() {
 	return (
 		<Fragment>
 			<Fonts />
 			<GlobalStyle />
-			<Container>
-				<Div>meow</Div>
-			</Container>
 		</Fragment>
 	)
 }
