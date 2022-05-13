82aa81ab8c4f4b551695e26e5fd55730a954a65d
tikkasandy <82177700+tikkasandy@users.noreply.github.com>
2022-05-13 23:24:43 +0300

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Fragment } from 'react'
 // import { ButtonAddTransactios } from 'modules'
+import { Logo } from 'modules'
 
 export default function App() {
-	return <Fragment>{/* <ButtonAddTransactios /> */}</Fragment>
+	return <Fragment>
+		{/* <ButtonAddTransactios /> */}
+		<Logo />
+	</Fragment>
 }
