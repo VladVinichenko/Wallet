ea458b94940d008be7753847e25075b6b25acb8c
vladislav-slobodenjuk <vladjun@gmail.com>
2022-05-14 23:39:58 +0300

    Formik stage

--- src/modules/components/addTransaction/addTransaction.jsx
+++ src/modules/components/addTransaction/addTransaction.jsx
@@ -1,99 +1,83 @@
 import { useState } from 'react'
 import Datetime from 'react-datetime'
-import { Formik } from 'formik'
-// import * as Yup from 'yup'
+import { Formik, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 
 import 'react-datetime/css/react-datetime.css'
-
 // import { sprite } from 'assets'
-// const func = () => {}
-// const = style.component``
 
 // const = style.component``
 
 export const AddTransaction = (toggleModal) => {
-	const [isIncome, setIsIncome] = useState(false)
-	console.log(isIncome)
-	const [category, setCategory] = useState('')
-	const [summ, setSumm] = useState()
 	const [date, setDate] = useState(Date.now()) //текущая дата
-	const [comment, setComment] = useState('')
 
-	const defineType = (isIncome) => {
-		return isIncome ? 'increment' : 'decrement'
-	}
-
-	const categories = () => {
-		const result = [1, 2, 3, 4, 5] // await fetch getCategories
-		return [null, ...result]
-	}
-
-	const handleTypeChange = ({ currentTarget: { checked } }) => {
-		setIsIncome(checked)
-		if (!checked) setCategory('')
+	const getCategories = () => {
+		const result = [
+			{ _id: 111, name: 'main' },
+			{ _id: 222, name: 'food' },
+			{ _id: 333, name: 'auto' },
+			{ _id: 444, name: 'development' },
+			{ _id: 555, name: 'kids' },
+		] // await fetch getCategories
+		return [...result]
 	}
 
-	const handleCategoryChange = ({ currentTarget: { value } }) => {
-		setCategory(value)
-	}
-	const handleSummChange = ({ currentTarget: { value } }) => {
-		setSumm(value)
-	}
+	const categories = ['', ...getCategories()]
+
 	const handletDateChange = ({ _d: time }) => {
-		// console.log(e)
-		// const time = e._d
-		// console.log(time.getTime())
-		setDate(time?.getTime())
+		// setDate(time?.getTime())
+		setDate(time)
 		console.log(time)
 	}
-	const handleCommentChange = ({ currentTarget: { value } }) => {
-		setComment(value)
+
+	const addTransaction = async (values) => {
+		if (!values.isConsumption) values.category = 'regular income'
+
+		const type = values.isConsumption ? 'decrement' : 'increment'
+		values = { type, ...values, date }
+		delete values.isConsumption
+		/*
+					добавить в базу
+					добавить в стор
+					очистить поля
+					закрыть модалку
+					*/
+
+		await new Promise((resolve) => setTimeout(resolve, 500))
+		alert(JSON.stringify(values, null, 2))
 	}
 
-	const addTransaction = async (e) => {
-		e.preventDefault()
-		console.log('add')
-		// console.log(e)
-		/*
-    добавить в базу
-    добавить в стор
-    очистить поля
-    закрыть модалку
-    */
-	}
-	//
+	const transactionSchena = Yup.object().shape({
+		isConsumption: Yup.boolean().required('Required'),
+		category: Yup.string(),
+		summ: Yup.number().required('Required'),
+		// date: Yup.date().required('Required').default(date),
+		comment: Yup.string(),
+	})
+
 	return (
 		<div className='addTransaction'>
 			<Formik
-				initialValues={{ isIncome, category, summ, date, comment }}
-				onSubmit={async (values) => {
-					console.log(values)
-					await sleep(500)
-					alert(JSON.stringify(values, null, 2))
-				}}
-				// validationSchema={Yup.object().shape({
-				// 	email: Yup.string().email().required('Required'),
-				// })}
+				initialValues={{ isConsumption: true, category: categories[0], summ: '', date, comment: '' }}
+				onSubmit={addTransaction}
+				validationSchema={transactionSchena}
 			>
-				{({ values }) => (
-					<form
-						className='transactionForm'
-						action='/users/current/transactions'
-						method='POST'
-						onSubmit={addTransaction}
-					>
-						<input type='checkbox' name='isIncome' value={values.isIncome} onChange={handleTypeChange} />
-						{isIncome && (
-							<select name='category' onChange={handleCategoryChange}>
-								{categories().map((category) => {
+				{({ values, touched, errors, dirty, isSubmitting, handleChange, handleBlur, handleSubmit, handleReset }) => (
+					<form className='transactionForm' onSubmit={handleSubmit}>
+						<Field type='checkbox' name='isConsumption' onChange={handleChange} />
+						{values.isConsumption && (
+							<select name='category' onChange={handleChange}>
+								{categories.map((category, index) => {
 									return (
-										<option value={category} key={category}>
-											{category}
+										<option value={category._id} key={index}>
+											{category.name}
 										</option>
 									)
 								})}
 							</select>
 						)}
+						{/* {errors.category && touched.category && <div className='input-feedback'>{errors.category}</div>} */}
+						{errors.category && touched.category && errors.category}
 						<input
 							type='number'
 							name='summ'
@@ -101,9 +85,11 @@ export const AddTransaction = (toggleModal) => {
 							step='0,01'
 							placeholder='0.00'
 							value={values.summ}
-							onChange={handleSummChange}
-							required
+							onChange={handleChange}
+							onBlur={handleBlur}
 						/>
+						{/* {errors.summ && touched.summ && errors.summ} */}
+						<ErrorMessage name='summ' component='div' />
 						<Datetime
 							name='date'
 							dateFormat='DD.MM.YYYY'
@@ -111,9 +97,20 @@ export const AddTransaction = (toggleModal) => {
 							initialValue={values.date}
 							onChange={handletDateChange}
 						/>
-						<input type='text' name='comment' value={values.comment} onChange={handleCommentChange} />
+						{errors.date && touched.date && errors.date}
+
+						<input
+							type='text'
+							name='comment'
+							value={values.comment}
+							placeholder='comment'
+							autoComplete='off'
+							onChange={handleChange}
+						/>
 						{/* <button type='submit' className={button}> */}
-						<button type='submit'>Add</button>
+						<button type='submit' disabled={isSubmitting}>
+							Add
+						</button>
 						{/* <button type='button' className={button} onClick={toggleModal}>  */}
 						<button type='button' onClick={() => console.log('cancel')}>
 							Cancel
