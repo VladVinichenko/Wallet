69e2f8969d96a6a48d7bcb7fc8f1aa9e1965e4d0
vladislav-slobodenjuk <vladjun@gmail.com>
2022-05-14 16:10:56 +0300

    stage without Formik

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Fragment } from 'react'
 // import { ButtonAddTransactios } from 'modules'
 import { AddTransaction } from 'modules/components/addTransaction'
+import 'react-datetime/css/react-datetime.css'
+
 // исправить импорт
 
 export default function App() {
--- src/modules/components/addTransaction/addTransaction.jsx
+++ src/modules/components/addTransaction/addTransaction.jsx
@@ -1,29 +1,36 @@
 import { useState } from 'react'
+import Datetime from 'react-datetime'
+import { Formik } from 'formik'
+// import * as Yup from 'yup'
+
+import 'react-datetime/css/react-datetime.css'
 
 // import { sprite } from 'assets'
-// import
-// const
 // const func = () => {}
 // const = style.component``
-// export const component = () => {return()}
 
 // const = style.component``
 
-export const AddTransaction = async (toggleModal) => {
+export const AddTransaction = (toggleModal) => {
 	const [isIncome, setIsIncome] = useState(false)
 	console.log(isIncome)
 	const [category, setCategory] = useState('')
-	const [summ, setSumm] = useState('')
-	const [date, setDate] = useState('') //текущая дата
+	const [summ, setSumm] = useState()
+	const [date, setDate] = useState(Date.now()) //текущая дата
 	const [comment, setComment] = useState('')
 
+	const defineType = (isIncome) => {
+		return isIncome ? 'increment' : 'decrement'
+	}
+
 	const categories = () => {
-		//fetch getCategories
-		return [1, 2, 3, 4, 5]
+		const result = [1, 2, 3, 4, 5] // await fetch getCategories
+		return [null, ...result]
 	}
 
 	const handleTypeChange = ({ currentTarget: { checked } }) => {
 		setIsIncome(checked)
+		if (!checked) setCategory('')
 	}
 
 	const handleCategoryChange = ({ currentTarget: { value } }) => {
@@ -32,16 +39,21 @@ export const AddTransaction = async (toggleModal) => {
 	const handleSummChange = ({ currentTarget: { value } }) => {
 		setSumm(value)
 	}
-	const handletDateChange = ({ currentTarget: { value } }) => {
-		setDate(value)
+	const handletDateChange = ({ _d: time }) => {
+		// console.log(e)
+		// const time = e._d
+		// console.log(time.getTime())
+		setDate(time?.getTime())
+		console.log(time)
 	}
 	const handleCommentChange = ({ currentTarget: { value } }) => {
 		setComment(value)
 	}
 
-	const addTransaction = (e) => {
+	const addTransaction = async (e) => {
 		e.preventDefault()
 		console.log('add')
+		// console.log(e)
 		/*
     добавить в базу
     добавить в стор
@@ -52,45 +64,63 @@ export const AddTransaction = async (toggleModal) => {
 	//
 	return (
 		<div className='addTransaction'>
-			<form className='transactionForm' action='/users/current/transactions' method='POST' onSubmit={addTransaction}>
-				<input type='checkbox' name='isIncome' value={isIncome} onChange={handleTypeChange} />
-				{isIncome && (
-					<select name='category' onChange={handleCategoryChange}>
-						{categories().map((category) => {
-							return (
-								<option value={category} key={category}>
-									{category}
-								</option>
-							)
-						})}
-					</select>
+			<Formik
+				initialValues={{ isIncome, category, summ, date, comment }}
+				onSubmit={async (values) => {
+					console.log(values)
+					await sleep(500)
+					alert(JSON.stringify(values, null, 2))
+				}}
+				// validationSchema={Yup.object().shape({
+				// 	email: Yup.string().email().required('Required'),
+				// })}
+			>
+				{({ values }) => (
+					<form
+						className='transactionForm'
+						action='/users/current/transactions'
+						method='POST'
+						onSubmit={addTransaction}
+					>
+						<input type='checkbox' name='isIncome' value={values.isIncome} onChange={handleTypeChange} />
+						{isIncome && (
+							<select name='category' onChange={handleCategoryChange}>
+								{categories().map((category) => {
+									return (
+										<option value={category} key={category}>
+											{category}
+										</option>
+									)
+								})}
+							</select>
+						)}
+						<input
+							type='number'
+							name='summ'
+							min='0'
+							step='0,01'
+							placeholder='0.00'
+							value={values.summ}
+							onChange={handleSummChange}
+							required
+						/>
+						<Datetime
+							name='date'
+							dateFormat='DD.MM.YYYY'
+							timeFormat={false}
+							initialValue={values.date}
+							onChange={handletDateChange}
+						/>
+						<input type='text' name='comment' value={values.comment} onChange={handleCommentChange} />
+						{/* <button type='submit' className={button}> */}
+						<button type='submit'>Add</button>
+						{/* <button type='button' className={button} onClick={toggleModal}>  */}
+						<button type='button' onClick={() => console.log('cancel')}>
+							Cancel
+						</button>
+					</form>
 				)}
-				<input
-					type='number'
-					name='summ'
-					min='0'
-					step='0,01'
-					placeholder='0.00'
-					required
-					value={summ}
-					onChange={handleSummChange}
-				/>
-				<input type='date' name='date' value={date} onChange={handletDateChange} />
-				{/* // date library */}
-				<input type='text' name='comment' value={comment} onChange={handleCommentChange} />
-				{/* <button type='submit' className={button}> */}
-				<button type='submit'>Add</button>
-				{/* <button type='button' className={button}> */}
-				<button
-					type='button'
-					onClick={() => {
-						console.log('cancel')
-					}}
-				>
-					{/* <button type='button' onClick={toggleModal}> */}
-					Cancel
-				</button>
-			</form>
+			</Formik>
 		</div>
 	)
 }
