07acaa80ca6901fc713fe8397532abb96bcdcdee
tikkasandy <82177700+tikkasandy@users.noreply.github.com>
2022-05-18 19:19:25 +0300

    Add props

--- src/modules/common/CustomLoader/index.jsx
+++ src/modules/common/CustomLoader/index.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
-import {Oval} from 'react-loader-spinner'
+import {RotatingLines} from 'react-loader-spinner'
 import { vars } from 'stylesheet';
 
 const Loader = styled.div`
-    position: fixed;
+    position: ${props => props.inBlock ? "absolute" : "fixed"};
     z-index: 1500;
     top: 50%;
     left: 50%;
@@ -11,14 +11,12 @@ const Loader = styled.div`
     transform: translate(-50%, -50%);
 `
 
-export const CustomLoader = () => {
+export const CustomLoader = ({inBlock, size, color}) => {
   return (
-    <Loader>
-      <Oval 
-      color={vars.color.accent.primary} 
-      secondaryColor={vars.color.accent.seconary}
-      height={100} 
-      width={100} />
+    <Loader inBlock={inBlock}>
+      <RotatingLines
+      strokeColor={color ? color : vars.color.accent.primary} 
+      width={size ? size : 100} />
     </Loader>
   );
 };
