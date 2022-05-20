3c2f5fc3e11f47246a8ae47ddd8f1804b6b61e28
Yevheniia Mankivska <evgeniyamankovskaya@gmail.com>
2022-05-20 11:40:35 +0300

    add 404

--- src/modules/pages/notFoundPage/index.js
+++ src/modules/pages/notFoundPage/index.js
@@ -1,18 +1,41 @@
-import { NotFound } from 'modules/components'
-import { Container } from 'modules/common'
+import { useNavigate } from 'react-router-dom'
+import notFoundImage from 'assets/images/notFound/notFoundImage.webp'
+import { Button } from 'modules/common'
 import styled from 'styled-components'
+import { Container } from 'modules/common'
 
-const ImageWrapper = styled.div`
+const PageWrapper = styled.div`
 	position: fixed;
 	width: 100vw;
 	height: 100vh;
-	z-index: 1000;
+`
+const ImageWrapper = styled.div`
+	background-image: url(${notFoundImage});
+	height: 100%;
+	width: 100%;
+	background-size: contain;
+	background-repeat: no-repeat;
+	background-position: center;
+	background-color: rgba(255, 255, 255, 0.4);
+	backdrop-filter: blur(50px);
 `
 
 export const NotFoundPage = () => {
+	const navigate = useNavigate()
+	const backHome = () => {
+		navigate('/')
+	}
 	return (
-		<div>
-			<NotFound />
-		</div>
+		<>
+			<PageWrapper>
+				<ImageWrapper>
+					<Container>
+						<Button onClickButton={backHome} type='button' title={'back home'} label={'back home'}>
+							Back home
+						</Button>
+					</Container>
+				</ImageWrapper>
+			</PageWrapper>
+		</>
 	)
 }
