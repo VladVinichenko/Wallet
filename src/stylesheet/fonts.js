a48202155e75ff9e4ad89f0658041036e6ece4ad
tikkasandy <82177700+tikkasandy@users.noreply.github.com>
2022-05-21 10:37:08 +0300

    Delete extra fonts

--- src/stylesheet/fonts.js
+++ src/stylesheet/fonts.js
@@ -2,8 +2,6 @@ import { createGlobalStyle } from 'styled-components'
 import CirceRegular from '../assets/fonts/Circe-Regular.otf'
 import CirceBold from '../assets/fonts/Circe-Bold.otf'
 import PoppinsRegular from '../assets/fonts/Poppins-Regular.ttf'
-import PoppinsMedium from '../assets/fonts/Poppins-Medium.ttf'
-import PoppinsSemiBold from '../assets/fonts/Poppins-SemiBold.ttf'
 import PoppinsBold from '../assets/fonts/Poppins-Bold.ttf'
 import Segoeui from '../assets/fonts/segoeui.ttf'
 import Segoeuib from '../assets/fonts/segoeuib.ttf'
@@ -27,20 +25,6 @@ export const Fonts = createGlobalStyle`
   font-family: 'Poppins';
   src: url(${PoppinsRegular}) format('truetype');
   font-weight: 400;
-  font-style: normal;
-}
-
-@font-face {
-  font-family: 'Poppins';
-  src: url(${PoppinsMedium}) format('truetype');
-  font-weight: 500;
-  font-style: normal;
-}
-
-@font-face {
-  font-family: 'Poppins';
-  src: url(${PoppinsSemiBold}) format('truetype');
-  font-weight: 600;
   font-style: normal;
 }
 
