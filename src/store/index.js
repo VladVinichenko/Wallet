dae63ac7f9b650c4b186e34901071ca63124d60b
VladVinichenko <vinnyxxxl925@gmail.com>
2022-05-22 21:52:08 +0300

    c1

--- src/store/index.js
+++ src/store/index.js
@@ -14,8 +14,8 @@ import { CustomLoader } from '../modules/common/CustomLoader'
 import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, REGISTER, PURGE, PERSIST } from 'redux-persist'
 
 // axios.defaults.baseURL = REACT_APP_API_URL
-axios.defaults.baseURL = 'https://wallet-api-goit.herokuapp.com/api/'
-// axios.defaults.baseURL = 'http://localhost:3001/api/'
+// axios.defaults.baseURL = 'https://wallet-api-goit.herokuapp.com/api/'
+axios.defaults.baseURL = 'http://localhost:3005/api/'
 
 export const token = {
 	set(token) {
@@ -30,7 +30,7 @@ export const token = {
 const authPersistConfig = {
 	key: 'auth',
 	storage,
-	whitelist: ['accessToken', 'refreshToken'],
+	whitelist: ['accessToken'],
 }
 
 const logger = null
