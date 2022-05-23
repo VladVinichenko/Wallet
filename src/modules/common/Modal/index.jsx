ddf97d7fef24358c8becbc4aa244dd46e124f657
VladVinichenko <vinnyxxxl925@gmail.com>
2022-05-23 20:37:54 +0300

    c1

--- src/modules/common/Modal/index.jsx
+++ src/modules/common/Modal/index.jsx
@@ -9,12 +9,12 @@ import { setCloseModal } from 'store'
 const modalRoot = document.querySelector('#root-modal')
 
 export const Backdrop = styled.div`
+	z-index: 10000;
 	position: fixed;
 	top: 0;
 	left: 0;
 	bottom: 0;
 	right: 0;
-	z-index: 1200;
 	display: flex;
 	justify-content: center;
 	align-items: center;
